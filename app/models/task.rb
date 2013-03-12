d46409245954329678c636a670aae786e6a1fcb9
Mark Hoad <markdhoad@gmail.com>
2013-03-12 21:00:37 +1100

    Working MVP

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ActiveRecord::Base
-  attr_accessible :doability_score, :free, :impact_score, :name, :paid, :reqruires_developer, :requires_client, :risk_score, :time_taken, :total_score
+  attr_accessible :doability_score, :free, :impact_score, :name, :paid, :reqruires_developer, :requires_client, :risk_score, :time_taken, :total_score, :category_id
   belongs_to :category
 
   validates :impact_score, :name, :risk_score, :time_taken, :presence => true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130312061638) do
+ActiveRecord::Schema.define(:version => 20130312064806) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(:version => 20130312061638) do
     t.decimal  "total_score"
     t.datetime "created_at",          :null => false
     t.datetime "updated_at",          :null => false
+    t.integer  "category_id"
   end
 
 end
