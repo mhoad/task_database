ef972a86c86cf7e54264403955346afe038c92c4
Mark Hoad <markdhoad@gmail.com>
2013-03-12 21:24:48 +1100

    Slightly improved MVP

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,11 @@
 class Task < ActiveRecord::Base
   attr_accessible :doability_score, :free, :impact_score, :name, :paid, :reqruires_developer, :requires_client, :risk_score, :time_taken, :total_score, :category_id
   belongs_to :category
-
+  before_save :calculate_total_score
   validates :impact_score, :name, :risk_score, :time_taken, :presence => true
+
+  private
+    def calculate_total_score
+      self.total_score = ((self.impact_score.to_f * self.doability_score.to_f) / self.risk_score.to_f).round(2)
+    end
 end
